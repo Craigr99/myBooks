8c086c8710260d9ebca4881ffc2c5c27b2c4f3c5
Craig <craigred99@gmail.com>
2020-11-09 15:41:11 +0000

    added some css scaffolding and edited login page style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,68 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card rounded">
+                    <div class="row vh-80">
+                        <div class="col-sm-6 col-md-6 p-5 d-none d-md-block red login-left-container">
+                            <h3>myBooks</h3>
+                        </div>
+
+                        <div class="col-md-6 px-md-5 px-lg-0">
+                            <div class="d-flex align-items-center justify-content-center h-100 ">
+                                <form action="{{ route('login') }}" method="POST">
+                                    @csrf
+                                    <h4>Log in</h4>
+                                    <p>Don’t have an account? Create one now </p>
+                                    <input id="username" type="text"
+                                        class="form-control @error('username') is-invalid @enderror" name="username"
+                                        value="{{ old('username') }}" required autofocus placeholder="Username">
+
+                                    @error('username')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+
+                                    <input id="password" type="password"
+                                        class="form-control mt-2 @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="current-password" placeholder="Password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+
+                                    <div class="d-flex align-items-center mt-3 mb-3">
+                                        <button type="submit" class="btn my-btn my-btn-primary mr-3">Login</button>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                                {{ old('remember') ? 'checked' : '' }}>
+
+                                            <label class="form-check-label" for="remember">
+                                                Remember password
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        @if (Route::has('password.request'))
+                                            <a href="{{ route('password.request') }}">
+                                                Forgot your password?
+                                            </a>
+                                        @endif
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        {{-- <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Login') }}</div>
@@ -20,9 +81,9 @@
                                         value="{{ old('username') }}" required autofocus>
 
                                     @error('username')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                     @enderror
                                 </div>
                             </div>
@@ -37,9 +98,9 @@
                                         required autocomplete="current-password">
 
                                     @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                     @enderror
                                 </div>
                             </div>
@@ -74,6 +135,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 @endsection
